d1e111c5f204b5dc72974881511f8af3fea569de
aseel-alsayeh <aseelalsayeh97@gmail.com>
2021-06-20 23:08:14 +0300

    solve lab01

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+ class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <p> The authour Name: Aseel Alsyaeh</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
