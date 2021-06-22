e0065ec9c5d8687a10b44f695ace67cb70375331
aseel-alsayeh <aseelalsayeh97@gmail.com>
2021-06-22 22:41:00 +0300

    not finish yet

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 import HornedBeast from './HornedBeast'
+import SelectedBeast from './SelectedBeast'
 import Data from './data.json'
 
 
 class Main extends Component {
+    
     render() {
         return (
             <div>
                 {
                     Data.map((b, index) => {
-                        return <HornedBeast key={index} clicks={0} title={b.title} imageUrl={b.image_url} description={b.description} />
+                        return <HornedBeast  key={index} clicks={0} title={b.title} imageUrl={b.image_url} description={b.description} />
                     })
                 }
-                
+                <SelectedBeast/>
             </div>
 
         )
---
+++ src/SelectedBeast.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Button,Modal } from 'react-bootstrap';
+
+
+ class SelectedBeast extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            width:props.width,
+        }
+    }
+    updateState=()=>{
+        this.setState(
+            {
+                width:"1000px"
+            })
+        }
+    render() {
+        return (
+            <div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+                
+            
+        )
+    }
+}
+
+export default SelectedBeast
